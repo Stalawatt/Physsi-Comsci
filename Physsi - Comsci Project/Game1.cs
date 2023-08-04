9410cd2e654ecbd800afab081012b364ea05303c
Stalawatt <joshuamrowell@gmail.com>
2023-08-04 20:31:10 +0100

    Working more on the homepage.
    Added Quit button
    Added Text to all buttons

--- Physsi - Comsci Project/Game1.cs
+++ Physsi - Comsci Project/Game1.cs
@@ -11,26 +11,59 @@ namespace Physsi___Comsci_Project
         private SpriteBatch _spriteBatch;
 
         // HOMESCREEN SPRITES LOADING TEXTURE2D
-        private Texture2D Rigid_Button;
+        private Texture2D RigidAndSoft_Button;
         private Texture2D Options_Button;
+        private Texture2D Quit_Button;
 
         // SPRITEFONTS FOR TEXT
-        private SpriteFont PhyssiLogo;
+        private SpriteFont Logo_Font;
+        private SpriteFont Large_Button_Font;
+        private SpriteFont Small_Button_Font;
 
         // TEXT 
-        private string PhyssiText = "Physsi"; // TITLE
-          
+        private string Physsi_Logo_RawText = "Physsi"; // TITLE
+
+        private string Rigid_Button_RawText = "Rigid-Body";
+        private string Options_Button_RawText = "Options";
+        private string Soft_Button_RawText = "Soft-Body";
+        private string Quit_Button_RawText = "Quit";
+
 
         // HOMESCREEN BUTTON POSITIONS
         Vector2 Rigid_Body_Pos = new Vector2(460, 400); // From top left of screen
         Vector2 Soft_Body_Pos = new Vector2(460, 600); // From top left of screen
         Vector2 Options_Button_Pos = new Vector2(585, 800); // From top left of screen
+        Vector2 Quit_Button_Pos = new Vector2(1700, 850); // From top left of screen
 
 
-        static class Physsi_Text_Width
+        static class Physsi_Text_Attr
         {
             public static float width;
-            public static Vector2 Physsi_Pos = new Vector2( 680, 50); // From top left of screen
+            public static Vector2 Position = new Vector2(680, 50); // From top left of screen
+        }
+
+        static class Rigid_Button_Text_Attr
+        {
+            public static float width;
+            public static Vector2 Position = new Vector2(650,400); // Center in the middle of the button
+        }
+
+        static class Soft_Button_Text_Attr
+        {
+            public static float width;
+            public static Vector2 Position = new Vector2(650, 600); // Center in the middle of the button
+        }
+
+        static class Options_Button_Text_Attr
+        {
+            public static float width;
+            public static Vector2 Position = new Vector2(775, 825); // Center in the middle of the button
+        }
+
+        static class Quit_Button_Text_Attr
+        {
+            public static float width;
+            public static Vector2 Position = new Vector2(1700,875); // Center in the middle of the button
         }
 
         public HomeScreen()
@@ -67,13 +100,14 @@ namespace Physsi___Comsci_Project
             // TODO: use this.Content to load your game content here
 
             // INITIALISE TEXTURES
-            Rigid_Button = Content.Load<Texture2D>("1000x174 Rigidbody Button");
+            RigidAndSoft_Button = Content.Load<Texture2D>("1000x174 Rigidbody Button");
             Options_Button = Content.Load<Texture2D>("Options button");
+            Quit_Button = Content.Load<Texture2D>("Quit_Button");
+            Logo_Font = Content.Load<SpriteFont>("Physsi");
+            Large_Button_Font = Content.Load<SpriteFont>("Large_Button");
+            Small_Button_Font = Content.Load<SpriteFont>("Small_Button");
 
-            PhyssiLogo = Content.Load<SpriteFont>("Physsi");
-
-
-            Physsi_Text_Width.width = PhyssiLogo.MeasureString("Physsi").X * 150;
+            Physsi_Text_Attr.width = Logo_Font.MeasureString("Physsi").X * 150;
 
 
             
@@ -96,11 +130,16 @@ namespace Physsi___Comsci_Project
             
 
             _spriteBatch.Begin();
-            _spriteBatch.Draw(Rigid_Button, Rigid_Body_Pos, Color.White);
-            _spriteBatch.Draw(Rigid_Button, Soft_Body_Pos, Color.White);
+            _spriteBatch.Draw(RigidAndSoft_Button, Rigid_Body_Pos, Color.White);
+            _spriteBatch.Draw(RigidAndSoft_Button, Soft_Body_Pos, Color.White);
             _spriteBatch.Draw(Options_Button, Options_Button_Pos, Color.White);
-            _spriteBatch.DrawString(PhyssiLogo, PhyssiText, Physsi_Text_Width.Physsi_Pos, Color.White);
-    
+            _spriteBatch.Draw(Quit_Button, Quit_Button_Pos, Color.White);
+            _spriteBatch.DrawString(Logo_Font, Physsi_Logo_RawText, Physsi_Text_Attr.Position, Color.White);
+            _spriteBatch.DrawString(Large_Button_Font, Rigid_Button_RawText, Rigid_Button_Text_Attr.Position, Color.White);
+            _spriteBatch.DrawString(Large_Button_Font, Soft_Button_RawText, Soft_Button_Text_Attr.Position, Color.White);
+            _spriteBatch.DrawString(Small_Button_Font, Options_Button_RawText, Options_Button_Text_Attr.Position, Color.White);
+            _spriteBatch.DrawString(Small_Button_Font, Quit_Button_RawText, Quit_Button_Text_Attr.Position, Color.White);
+
 
             _spriteBatch.End();
 
