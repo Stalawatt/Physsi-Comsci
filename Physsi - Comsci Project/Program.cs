d6f308babcc63f1732339e274f18963c5fcf8902
Stalawatt <joshuamrowell@gmail.com>
2023-12-06 22:37:47 +0000

    Bug Fixes and adding first part of adding multiple scenes

--- Physsi - Comsci Project/Program.cs
+++ Physsi - Comsci Project/Program.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 
 var scene = new Physsi___Comsci_Project.SceneLoaded();
-using var game = new Physsi___Comsci_Project.Main();
 scene.scene_list_load();
 scene.check_errors();
 game.Run();
